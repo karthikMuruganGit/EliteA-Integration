56befc37b7eb24cf4fa78d4b0156f06d8e0585bd
karthikMuruganGit <karthikshivan08@gmail.com>
2026-03-05 14:41:05 +0530

    Delete tests/doc-menu.spec.ts

--- tests/doc-menu.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-// Test: Navigate to https://elitea.ai/doc and print menu items
-test('Print menu items on /doc', async ({ page }) => {
-  // TEST_BODY_START
-});
